c016be2c5033a7a7b3f8574080b4bc29f3647558
mintomohan <mintomohan@gmail.com>
2024-05-15 20:20:33 +0900

    debugging issue

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-#import bedrock_bot as bb
+import bedrock_bot as bb
 import os
 import json
 from flask import Flask
@@ -8,10 +8,8 @@ app = application
 
 status_code = 'INITIATED'
 
-'''
 status_code = bb.prepare_vectordb('Paris')
 bot = bb.create_chain()
-'''
 
 @application.route('/', methods=["GET"])
 def default():
@@ -23,16 +21,14 @@ def check_status():
     response = {'status' : status_code}
     return json.dumps(response)
 
-'''
 
-@application.route('/ask', methods=["GET"])
+@application.route('/chat', methods=["GET"])
 def chat():
     reply = bot.invoke('What is the population of Paris?')
     response = {'message' : reply}
     return json.dumps(response)
 
 
-'''
 
 @application.route('/secret', methods=["GET"])
 def check_secret():
